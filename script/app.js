0a3a80a3f28f5ead0d32059cb81ef0f8a877a9a0
Maplewing <sinmaplewing@gmail.com>
2013-09-02 00:13:11 +0800

    Add Loading screen.

--- script/app.js
+++ script/app.js
@@ -47,6 +47,7 @@ $(document).ready(function(){
 	$('#inputForm').submit(function(){ return false; });
 	
 	$('#getAllArticle').click(function(){
+		$('.loading').show("slow");
 		$('#result').hide("slow", function(){
 			$.get( "http://www.wretch.cc/blog/" + $('#username').val() + "%26list%3D1", function(data){
 				data = data.responseText;
@@ -57,7 +58,8 @@ $(document).ready(function(){
 					var tdContent = $(this).find("td").html();
 					$('#result ul').append("<li>" + tdContent + "</li>");
 				});
-				$('#result').show("slow");
+				$('.loading').hide("slow", function(){ $('#result').show("slow") });
+				
 			});
 		});
 		return false;
@@ -65,31 +67,33 @@ $(document).ready(function(){
 	
 	$('#getAtomFormat').click(function(){
 		var username = $('#username').val();
-		$.get( "http://www.wretch.cc/blog/" + username + "%26list%3D1", function(data){
-			data = data.responseText;
-			$('#result').hide("slow",function(){
-				$('#result').html("<h2>RAW Data：（每五秒抓一篇）</h2><p>完成會跳出存檔視窗</p><textarea />");
-				$('#result').show("slow");
-				var $display = $('#result textarea');
-				var RSSContent = '<?xml version="1.0" encoding="UTF-8"?><wretch>';
-				var $content = $(data).find(".blogbody table tr");
-				$content.each(function(index){
-					var self = this;
-					setTimeout( function(){
-					var aContent = $(self).find("a");
-					$.get( $(aContent).attr("href"), function(data){
-							RSSContent += "<page>";
-							RSSContent += escapeHTML(data.responseText);
-							RSSContent += "</page>";
-							if(index >= $content.length-1){
-								RSSContent += "</wretch>";
-								saveTextAsFile(RSSContent, (new Date())+"_"+username+".xml");
-							}
-							$display.html(escapeHTML(RSSContent));
-					});
-				}, index * 5000);});
-				});
-		});	
+		$('.loading').show("slow");
+		$('#result').hide("slow",function(){
+			$.get( "http://www.wretch.cc/blog/" + username + "%26list%3D1", function(data){
+				data = data.responseText;
+					$('#result').html("<h2>RAW Data：（每五秒抓一篇）</h2><p>完成會跳出存檔視窗</p><textarea />");
+					$('.loading').hide("slow", function(){ $('#result').show("slow") });
+					var $display = $('#result textarea');
+					var RSSContent = '<?xml version="1.0" encoding="UTF-8"?><wretch>';
+					var $content = $(data).find(".blogbody table tr");
+					$content.each(function(index){
+						var self = this;
+						setTimeout( function(){
+						var aContent = $(self).find("a");
+						$.get( $(aContent).attr("href"), function(data){
+								RSSContent += "<page>";
+								RSSContent += escapeHTML(data.responseText);
+								RSSContent += "</page>";
+								if(index >= $content.length-1){
+									RSSContent += "</wretch>";
+									saveTextAsFile(RSSContent, (new Date())+"_"+username+".xml");
+								}
+								$display.html(escapeHTML(RSSContent));
+						});
+					}, index * 5000);});
+					
+			});	
+		});
 		return false;
 	});
 });
